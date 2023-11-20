d9a6dca66cd0463495b4cddddafca79aea3fd000
William <williamferreira.programador@gmail.com>
2023-11-20 13:56:13 -0300

    feat: add stripe webhook

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -1,13 +1,9 @@
 "use server";
 
+import { stripe } from "@/lib/stripe";
 import { CartProducts } from "@/providers/cart";
-import Stripe from "stripe";
 
 export default async function createCheckout(products: CartProducts[]) {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2023-10-16",
-  });
-
   const checkout = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
