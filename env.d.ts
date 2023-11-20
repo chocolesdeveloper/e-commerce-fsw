df92dc659c5e7ff4b50c63a452f4243ffc51c8aa
William <williamferreira.programador@gmail.com>
2023-11-20 15:52:05 -0300

    feat: add HOST_URL

--- env.d.ts
+++ env.d.ts
@@ -3,8 +3,10 @@ declare namespace NodeJS {
     DATA_BASE: string;
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
-    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
-    STRIPE_SECRET_KEY: string;
     STRIPE_WEBHOOK_SECRET_KEY: string;
+    STRIPE_SECRET_KEY: string;
+    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
+    NEXTAUTH_PUBLIC: string;
+    HOST_URL: string;
   }
 }
